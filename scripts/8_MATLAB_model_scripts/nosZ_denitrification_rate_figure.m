b3b44ec02009d675425a302855f3ca21a7dda3d9
spacocha <sprehei1@jhu.edu>
2025-10-09 14:18:16 -0400

    Scripts for revisions submitted on 10.08.25

--- scripts/8_MATLAB_model_scripts/nosZ_denitrification_rate_figure.m
+++ scripts/8_MATLAB_model_scripts/nosZ_denitrification_rate_figure.m
@@ -10,49 +10,54 @@
 %cat TPM_final_table.KO.neg.good.taxa.txt | grep "K00376" | grep "Bacteroidetes" >> nosZ_Bacteroidetes.txt
 %In excel nosZ_by_taxa.xlsx, make just the headers we want and sum all
 %save as just the sum for Bacteroidetes\nGammas
-nosZ_taxa=readmatrix("nosZ_gamma_bacteroidetes.txt");
-nosZbactert=log(nosZ_taxa(1,:))';
-nosZgammat=log(nosZ_taxa(2,:)+0.01)';
+%divide by housekeeping genes...
 
+Cruise=(metadata.Yearday<125)*1+(metadata.Yearday>125&metadata.Yearday<175)*2+(metadata.Yearday>175&metadata.Yearday<225)*3+(metadata.Yearday>225)*4;
+Surf=metadata.Depth<2;
+Bot=metadata.Depth>2;
 
 
 %snp_hydro
 clf
 colormap default
-subplot(221)
-scatter(metadata.DO,nosZt,40,metadata.Yearday,'filled')
-xlabel('log Obs. Oxygen (\muM)')
-ylabel('log Abundance in nRPKM')
-title('\rm(a) Obs \it{nosZ}\rm vs. Obs. DO,time')
+subplot(131)
+scatter(metadata.DO(Surf),nosZt(Surf),40,Cruise(Surf),'filled'); hold on;
+scatter(metadata.DO(Bot),nosZt(Bot),40,Cruise(Bot),'filled','d');
+xlabel('log Oxygen (\muM)')
+ylabel('log normalized relative abundance')
+title("(a) {\itnosZ} ({\itr}="+ num2str(corr_obs(33), '%5.2f') + ", {\itp}=" + num2str(pval(33), '%5.2f') + ")")
 %hc=colorbar('Location',['eastoutside']);
 grid on
 
 
-subplot(222)
-scatter(log(dno2_freference+dno3_freference),nosZt,40,metadata.Yearday,'filled')
-xlabel('log Denitrif. rate (mmol m^-^3 d^-^1)')
-ylabel('log Abundance in nRPKM')
-title('\rm(b) Obs \it{nosZ}\rm vs. Mod. Denitrif.,time')
+subplot(132)
+scatter(log(dno2_freference(Surf)+dno3_freference(Surf)),nosZgammat(Surf),40,Cruise(Surf),'filled'); hold on;
+scatter(log(dno2_freference(Bot)+dno3_freference(Bot)),nosZgammat(Bot),40,Cruise(Bot),'filled','d'); 
+xlabel('log denitrification (mmol m^{-3} day^{-1})')
+ylabel('log normalized relative abundance')
+title("(b) Gammaproteobacteria {\itnosZ} ({\itr}="+ num2str(corr_obs(35), '%5.2f') + ", {\itp}=" + num2str(pval(35), '%5.2f') + ")")
 %hc=colorbar('Location',['eastoutside']);
 grid on
 
-
-subplot(223)
-scatter(log(dno2_freference+dno3_freference),nosZgammat,40,metadata.Yearday,'filled')
-xlabel('log Denitrif rate (mmol m^-^3 d^-^1)')
-ylabel('log Abundance in nRPKM')
-title('\rm(c) Obs Gamma \it{nosZ}\rm vs. Mod. Denitrif.,time')
+subplot(133)
+scatter(log(dno2_freference(Surf)+dno3_freference(Surf)),nosZbactert(Surf),40,Cruise(Surf),'filled'); hold on;
+scatter(log(dno2_freference(Bot)+dno3_freference(Bot)),nosZbactert(Bot),40,Cruise(Bot),'filled','d');
+xlabel('log denitrification (mmol m^{-3} day^{-1})')
+ylabel('log normalized relative abundance')
+title("(c) Bacteroidetes {\itnosZ} ({\itr}="+ num2str(corr_obs(34), '%5.2f') + ", {\itp}=" + num2str(pval(34), '%5.2f') + ")")
 %hc=colorbar('Location',['eastoutside']);
 grid on
 
-subplot(224)
-scatter(log(dno2_freference+dno3_freference),nosZbactert,40,metadata.Yearday,'filled')
-xlabel('log Denitrif. rate (mmol m^-^3 d^-^1)')
-ylabel('log Abundance in nRPKM')
-title('\rm(d)Obs Bacteroid. \it{nosZ}\rm vs. Mod. Denitrif.,time')
-%hc=colorbar('Location',['eastoutside']);
-grid on
-
-hc1=colorbar;
+colormap("default")
+cmap=colormap;
+colormap(cmap([1 86 171 255],:))
+hc1=colorbar();
+caxis([0.5 4.5])
 hc1.Position =[0.05 0.35 0.02 0.3];
-ylabel(hc1,'decimal day');
+xlabel(hc1,'Cruise Month');
+labelsc={'Apr','Jun','Jul','Aug'}
+hc1.Label.Rotation=0;
+hc1.Label.Position=[0.08 0.35 0];
+hc1.Direction="reverse";
+hc1.Ticks=(1:4);
+hc1.TickLabels=labelsc;
